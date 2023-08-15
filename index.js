8b5308aa5937a1b811ba52300a7e4646451e4a3d
Hilda Li <hildal@hildal-c02cf193md6r.corp.uber.com>
2023-08-15 13:46:09 -0700

    “my initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken =
+"pk.eyJ1IjoiYmFiYnMiLCJhIjoiY2s1b2JoMjZvMGYydzNmbXAxMXp1NWZhZyJ9.LEHmtAFLAij67eF-54FjxA";
+const map = new mapboxgl.Map({
+   container: "map", // container ID
+   style: "mapbox://styles/babbs/clk2po6u801nc01no7x6c6pj4", // style URL
+   center: [-74.5, 40], // starting position [lng, lat]
+   zoom: 9, // starting zoom
+});
+
+
